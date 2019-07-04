2346d4f685a7ce7f85f072a5f73ff28a5980380e
john sarigamala <praisym15@gmail.com>
2019-07-04 16:29:50 -0400

    routes

--- routes/api/userAuth.js
+++ routes/api/userAuth.js
@@ -1,9 +1,9 @@
-const express = require("express"); 
+const express = require("express");
 const router = express.Router(); //creates routes object
 
 //@route    Get api/users/test
 //@desc     Tests users route
 //@access   Public
-router.get('/test', (req,resp) => resp.json({msg:"User works"})); //when localhost:5000/api/users/test is called, display the json msg  
+router.get("/test", (req, resp) => resp.json({ msg: "User works" })); //when localhost:5000/api/users/test is called, display the json msg
 
 module.exports = router;
